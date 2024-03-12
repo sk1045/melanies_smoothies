54245f38ad5c10d83d9c21a2fa38d920748982d1
sambathkumar <139935996+sk1045@users.noreply.github.com>
2024-03-12 12:56:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ name_on_order = st.text_input('Name on Smoothie: ')
 st.write('The name on your smoothie will be:', name_on_order)
 
 
-cnx = get_active_session()
+cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 
@@ -21,19 +21,20 @@ ingredients_list = st.multiselect(
     max_selections=5
 )
 
-if ingredients_list:
+    if ingredients_list:
     ingredients_string = ''
-    for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
+        for fruit_choosen in ingredients_list:
+        ingredients_string += fruit_choosen + ' '
     
     my_insert_stmt = f"""INSERT INTO smoothies.public.orders(ingredients, name_on_order)
                         VALUES ('{ingredients_string}', '{name_on_order}')"""
     
     time_to_insert = st.button('Submit Order')
 
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
+
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie Order has been placed!', icon="✅")
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.text)
