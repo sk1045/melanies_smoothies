b0b7ac9357ef1face6eb5a4cf6b9cd96d3d83fa7
sambathkumar <139935996+sk1045@users.noreply.github.com>
2024-03-12 13:00:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
 import requests
 
 # Write directly to the app
