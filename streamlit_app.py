40964ae25c2a0076aada6980c22880591faf18d9
sambathkumar <139935996+sk1045@users.noreply.github.com>
2024-03-12 13:07:12 +0530

    Update streamlit_app.py
    JSON into a Dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,4 +36,5 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie Order has been placed!', icon="✅")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+# st.text(fruityvice_response.json())
+fv_df=st.dataframe(data=fruityvice_response.json(), use_container_width=true)
