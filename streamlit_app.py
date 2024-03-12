0186e0edd2d39b2b95e6996ab3841e0c987e03b2
sambathkumar <139935996+sk1045@users.noreply.github.com>
2024-03-12 16:17:39 +0530

    Update streamlit_app.py
    add search on

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ st.write('The name on your smoothie will be:', name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('search_on'))
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
@@ -25,6 +25,8 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         st.subheader(fruit_chosen + ' Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
